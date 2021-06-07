676d094bebb4306af98ed5295771a277d703b505
vishwap321 <85471634+vishwap321@users.noreply.github.com>
2021-06-07 13:16:04 +0530

    Update FirstClass.java
    System.out.println("Hello3");

--- FirstClass.java
+++ FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
         System.out.println("Hello");
         System.out.println("Hello1");
         System.out.println("Hello2");
+        System.out.println("Hello3");
     }
 
 }
