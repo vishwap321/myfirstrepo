b347d16a77fe80e760308c61dd8f70e374b871d4
vishwap321 <vishwanatha321@gmail.com>
2021-06-07 13:09:55 +0530

    1st Commit

--- FirstClass.java
+++ FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hello1");
+        System.out.println("Hello2");
     }
 
 }
